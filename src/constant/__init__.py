690ce4bcfe5004144742255bb08bec66ba5497f4
raunakgupta123 <dhruvgupta26082006@gmail.com>
2024-12-22 22:45:24 +0530

    added modules

---
+++ src/constant/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+AWS_S3_BUCKET_NAME="wafer-fault"
+MONGO_DATABASE_NAME="pwskills"
+MONGO_COLLECTION_NAME="waferfault"
+
+TARGET_COLUMN="quality"
+MONGO_DB_URL="mongodb+srv://snshrivas:Snshrivas@cluster0.u46c4.mongodb.net/?retrywrites=true&w=majority"
+
+
+MONGO_FILE_NAME="model"
+MONGO_FILE_EXTENSION=".pk1"
